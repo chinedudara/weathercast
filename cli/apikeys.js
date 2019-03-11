164f31b8d6f26634ac3d497c8f3252f83ae70891
Chinedu Dara <darachinedu@gmail.com>
2019-03-11 05:59:34 +0100

    Temporarily commit sensitive data - local

---
+++ cli/apikeys.js
@@ -0,0 +1,15 @@
+
+  //  Maps Api Url: https://maps.googleapis.com/maps/api/geocode/json?address=[Append location address]&key=[Append Api Key]
+  //  My Google Developer Api Key: AIzaSyBleTNciXvf61oQu1bTPauOWvtsph35Hgo
+
+  //  DarkSky Weather Api Url: https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+  // DarkSky Api Key: e2651a350bb84c72283f84d63b3da253
+
+  // MapBox Geocoding Api Url: https://api.mapbox.com/geocoding/v5/mapbox.places/[Append location address].json?access_token=[Api Token]
+  // MapBox Api Token: pk.eyJ1IjoiamV0cm9uMTAxIiwiYSI6ImNqdDFvODMydzB2Z3I0M3RoamdxdTdzd3cifQ.DZ0tjR1_8IodTbwAHCn7HA
+
+module.exports.apiKeys = {
+  geocode: 'AIzaSyBleTNciXvf61oQu1bTPauOWvtsph35Hgo',
+  darksky: 'e2651a350bb84c72283f84d63b3da253',
+  mapbox: 'pk.eyJ1IjoiamV0cm9uMTAxIiwiYSI6ImNqdDFvODMydzB2Z3I0M3RoamdxdTdzd3cifQ.DZ0tjR1_8IodTbwAHCn7HA'
+}
