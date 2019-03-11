d000f73728e0b34c2a5fca675fc4e82450b9d056
Chinedu Dara <darachinedu@gmail.com>
2019-03-11 07:04:47 +0100

    Heroku filename case sensitivity fixed

--- cli/utils/weather.js
+++ cli/utils/weather.js
@@ -1,8 +1,8 @@
 const request = require('request');
-// const {apiKeys} = require('./../apiKeys');
+const {apiKeys} = require('./../apikeys');
 
 let fetchWeather = (lat, lng, callback) => {
-  const apiKey = 'e2651a350bb84c72283f84d63b3da253';
+  const apiKey = apiKeys.darksky;
   const excludeBlocks = '?exclude=minutely,hourly,daily,alerts,flags'
   request({
     url: `https://api.darksky.net/forecast/${apiKey}/${lat},${lng}${excludeBlocks}`,
